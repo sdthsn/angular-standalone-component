f5fe9f6019fec06f7f26b0527897084541a07141
Shahadat Hossain <sdthsn@outlook.com>
2022-12-17 00:39:05 -0800

    adds images

--- src/app/books.service.ts
+++ src/app/books.service.ts
@@ -15,28 +15,28 @@ export class BooksService {
       id: 1,
       description: 'Book description 1',
       imageUrl:
-        'https://cdn.pixabay.com/photo/2015/06/02/12/59/book-794978_960_720.jpg',
+        './assets/book.jpg',
     },
     {
       name: 'Book 2',
       id: 2,
       description: 'Book description 2',
       imageUrl:
-        'https://cdn.pixabay.com/photo/2015/06/02/12/59/book-794978_960_720.jpg',
+        './assets/book.jpg',
     },
     {
       name: 'Book 3',
       id: 3,
       description: 'Book description 3',
       imageUrl:
-        'https://cdn.pixabay.com/photo/2015/06/02/12/59/book-794978_960_720.jpg',
+        './assets/book.jpg',
     },
     {
       name: 'Book 4',
       id: 4,
       description: 'Book description 4',
       imageUrl:
-        'https://cdn.pixabay.com/photo/2015/06/02/12/59/book-794978_960_720.jpg',
+        './assets/book.jpg',
     },
   ];
   constructor() {}
